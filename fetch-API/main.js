2b2b5c73cbdc9273bf1cd1c8781706db5f628afa
usmonzo <turaef.usmon@gmail.com>
2022-09-02 05:17:24 +0500

    Adding new mini-online shop with fetch()

--- fetch-API/main.js
+++ fetch-API/main.js
@@ -1,6 +1,6 @@
 fetch("https://reqres.in/api/users?page=2")
   .then((data) => {
-    //   console.log(data);
+    // console.log(data.json());
     return data.json();
   })
   .then((userdata) => {
@@ -17,4 +17,35 @@ fetch("https://reqres.in/api/users?page=2")
   })
   .catch((er) => {
     console.log(er);
+  });
+
+fetch("https://dummyjson.com/products")
+  .then((product) => {
+    // console.log(product.json());
+    return product.json();
+  })
+  .then((shop) => {
+    let productOne = "";
+    shop.products.map((info) => {
+      productOne += `<div class="product">
+      <div class="info">
+      <div class="product_name">${info.title}</div>
+      <div class="product_desc">${"Description:" + " " + info.description}</div>
+      <div class="product_price">${"Price:" + " " + info.price + "$"}</div>
+      <div class="product_rating">${"Rating:" + " " + info.rating}</div>
+      <div class="product_brand">${"Brand:" + " " + info.brand}</div>
+      <div class="category">${"Category:" + " " + info.category}</div>
+      </div>
+        <div class="product_images">
+            <img src="${info.images[0]}" alt="">
+            <img src="${info.images[1]}" alt="">
+            <img src="${info.images[2]}" alt="">
+            <img src="${info.images[3]}" alt="">
+        </div>
+        </div>`;
+    });
+    document.getElementById("mini_online_shop").innerHTML = productOne;
+  })
+  .catch((err) => {
+    console.log(err);
   });
